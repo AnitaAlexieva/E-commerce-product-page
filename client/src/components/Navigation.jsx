d7de3c4e900ac087c234df817b6c2fbed5e9fb11
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-05-23 09:23:42 +0300

    Change the color of the li elements in pages navigation

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -10,7 +10,7 @@ export default function Navigation() {
           <li>Contact</li>
         </ul>
         <ul className="profile">
-          <li><img src="\assets\images\icon-cart.svg" alt="basket" /></li>
+          <li><img className="bacsket" src="\assets\images\icon-cart.svg" alt="basket" /></li>
           <li><img className="avatar" src="\assets\images\image-avatar.png" alt="person-photo" /></li>
         </ul>
       </nav>
