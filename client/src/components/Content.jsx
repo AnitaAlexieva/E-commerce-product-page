44a96b42af822dfe89446d14749e092871a30607
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-05-26 15:44:49 +0300

    Make the count of the item to be  changed dinamicaly

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react"
+
 export default function Content() {
+  const [count, setCount] = useState(0)
+
+  const isUndurNull = () =>{
+    if(count <= 0){
+      setCount(0);
+    }else{
+      setCount(count-1);
+    }
+  }
     return(
         <div className="content">
         <div className="sneakers">
@@ -22,11 +33,11 @@ export default function Content() {
           <h6>$250.00</h6>
           <div className="buying">
             <div className="count">
-              <img className="minus" src="\assets\images\icon-minus.svg" alt="" />
-              <p className="count-number">0</p>
-              <img className="plus" src="\assets\images\icon-plus.svg" alt="" />
+              <button><img className="minus" onClick={isUndurNull} src="\assets\images\icon-minus.svg" alt="" /></button>
+              <p className="count-number">{count}</p>
+              <button><img className="plus" onClick={() => setCount(count+1)} src="\assets\images\icon-plus.svg" alt="" /></button>
             </div>
-            <button className="add-cart"><img src="\assets\images\icon-cart.svg" alt="" />Add to cart</button>
+            <button onClick={()=>setCount(count+1)} className="add-cart"><img src="\assets\images\icon-cart.svg" alt="" />Add to cart</button>
           </div>
         </div>
       </div>
